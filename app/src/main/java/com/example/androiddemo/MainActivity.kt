c4d7342799ef02ca3dac3e86094b31c7774e97fa
cag2050 <cag2050@163.com>
2021-06-09 12:04:50 +0800

    WindowManager.LayoutParams demo

--- app/src/main/java/com/example/androiddemo/MainActivity.kt
+++ app/src/main/java/com/example/androiddemo/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.androiddemo
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.PixelFormat
 import android.os.Bundle
 import android.util.Log
+import android.view.WindowManager
+import android.widget.RelativeLayout
+import androidx.appcompat.app.AppCompatActivity
 import com.example.androiddemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -35,6 +39,24 @@ class MainActivity : AppCompatActivity() {
         binding.refresh.setOnClickListener {
             val intent = Intent(this, MediaScannerConnectionActivity::class.java);
             startActivity(intent)
+        }
+
+        binding.layoutParams.setOnClickListener {
+            val backgroundLP = WindowManager.LayoutParams(
+                WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.LAST_APPLICATION_WINDOW,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE or WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSPARENT
+            )
+            val windowManager = windowManager
+            var backgroundRL = RelativeLayout(applicationContext)
+//            backgroundRL.setBackgroundColor(Color.parseColor("#66000000"))
+            backgroundRL.setBackgroundColor(Color.BLACK)
+//          十六进制透明度66，转为十进制是102
+            backgroundRL.background.mutate().alpha = 102
+//            backgroundRL.addView(container)
+            windowManager.addView(backgroundRL, backgroundLP)
         }
     }
 }
